18a9cd5ea5a1808c618b137a7860df3ec3621c23
Sasha Good <eiroglif@mail.ru>
2019-04-29 18:42:36 +0300

    clean code

--- hammingECC.py
+++ hammingECC.py
@@ -12,22 +12,17 @@ ECC_POS21 = ([2,4,6,8,10,12,14,16,18,20], [2,5,6,9,10,13,14,17,18],
 POSITION21 = (0,1,3,7,15)
 
 def is_bool(variable):
-    old_bool = variable
-    new_bool = [x for x in old_bool if x in ["0","1"]]
-    if len(old_bool) != len(new_bool):
-        return False
-    else:
-        return True
+    new_bool = [x for x in variable if x in ["0","1"]]
+    return len(variable) == len(new_bool)
     
 def counter(position, variable):
     list_bool = list(str(variable))    
     ecc_value = []
 
     for item_on_place in position:
-        new_list = item_on_place
         y = 0
-        for x in new_list:
-            if int(list_bool[int(x)]) == 1:
+        for x in item_on_place:
+            if list_bool[x] == "1":
                 y += 1
         if y % 2 == 0:
             ecc_value.append(0)
@@ -35,13 +30,12 @@ def counter(position, variable):
             ecc_value.append(1)
     return ecc_value
   
-def to_hemming(variable, position = POSITION16_5):
-    bit16 = str(variable)
-    
+def to_hemming(value, position = POSITION16_5):
+    bit16 = str(value)
     if len(bit16) != 16:
-        return ("Error кооличество разрядов не равно 16")
+        raise ValueError("Error кооличество разрядов не равно 16")
     elif is_bool(bit16) != True:
-        return ("Число не двоичное")
+        raise ValueError("Число не двоичное")
 
     ecc_date = counter(ECC_POS, bit16)
     print(ecc_date)
@@ -53,40 +47,35 @@ def to_hemming(variable, position = POSITION16_5):
     bit21 = ''.join(bit16)
     return(bit21)
 
+def checker(variable, ecc21_date):
+    list_21 = list(str(variable))
+    ecc_old = []
+    for x in POSITION21:
+        a = list_21[x]
+        ecc_old.append(int(a))
+    error_list = []
+    for i, x in enumerate(ecc_old):
+        if ecc21_date[i] != x:
+            error_list.append(i)
+    return error_list
+
 def to_16bit(variable):
-    def checker(variable, ecc21_date):
-        list_21 = list(str(variable))
-        ecc_old = []
-        for x in POSITION21:
-            a = list_21[x]
-            ecc_old.append(int(a))
-        error_list = []
-        for x in range(0,5):
-            if ecc21_date[x] == ecc_old[x]:
-                pass
-            else:    
-                error_list.append(x)
-        return error_list
-
-
     bit21ecc = str(variable)
     
     if len(bit21ecc) != 21:
-        return ("Error число разрядов не 21")
+        raise ValueError("Error число разрядов не 21")
     elif is_bool(bit21ecc) != True:
-        return ("Число не двоичное")
+        raise ValueError("Число не двоичное")
 
     ecc21_date = counter(ECC_POS21, bit21ecc)
     err_list = checker(bit21ecc, ecc21_date)
 
     bit21ecc = list(bit21ecc)
     if err_list:
-        print("1 error found")
         err_pos = 0
         for x in err_list:
             err_pos += (POSITION21[x] + 1)
         err_pos -= 1    
-        print("Error fix. Позиция ошибки:", err_pos)
         if bit21ecc[err_pos] == 0:
             del bit21ecc[err_pos]
             bit21ecc.insert(err_pos, '1')
@@ -118,16 +107,22 @@ def test():
     habra_word_21_test = "100110000100001011101"
 
     print("\nCoder test")
-    k = to_hemming(a)
-    print(k)
+    try:
+        k = to_hemming(a)
+    except ValueError as e:
+        print(e)
     k = to_hemming(num1)
     print(k)
-    k = to_hemming(num2)
-    print(k)
+    try:
+        k = to_hemming(num2)
+    except ValueError as e:
+        print(e)
     k = to_hemming(num3)
     print(k)
-    k = to_hemming(num4)
-    print(k)
+    try:
+        k = to_hemming(num4)
+    except ValueError as e:
+        print(e)
     print("habra_word ", habra_word)
     habra_word_21 = to_hemming(habra_word)
     print(habra_word_21)
@@ -136,13 +131,19 @@ def test():
     print("Значение равно ", ecc1)
     g = to_16bit(ecc1)
     print(g)
-    g = to_16bit(ecc2)
-    print(g)
+    try:
+        g = to_16bit(ecc2)
+        print(g)
+    except ValueError as e:
+        print(e)
     print("Значение равно ", ecc3)
+    
     g = to_16bit(ecc3)
     print(g)
-    g = to_16bit(ecc4)
-    print(g)
+    try:
+        g = to_16bit(ecc4)
+    except ValueError as e:
+        print(e)
     habra_word_16b = to_16bit(habra_word_21)
     print("habra_word 16bit ", habra_word_16b)
 
@@ -150,36 +151,4 @@ def test():
     print("Проверка перевода")
     print("Сравним хабра слова ", habra_word == habra_word_16b )
 
-test()    
-
-
-"""
-trash
-    position1 = (1, 2, 4, 5, 7, 9, 11, 12, 14, 16)
-    position2 = (1, 3, 4, 6, 7, 10, 11, 13, 14)
-    position4 = (2, 3, 4, 8, 9, 10, 15, 16)
-    position8 = (5, 6, 7, 8, 9, 10, 11)
-    position16 = (12, 13, 14, 15, 16)
-               0123456789ABCDEF
-       
-                  
-
-    position2 = (1, 3, 4, 6, 7, 10, 11, 13, 14)
-    position4 = (2, 3, 4, 8, 9, 10, 15, 16)
-    position8 = (5, 6, 7, 8, 9, 10, 11)
-    position16 = (12, 13, 14, 15, 16)
-
-
-def counter(position, variable):
-    list_bool = list(str(variable))
-    place = list(position)
-    y = 0  
-    for x in place:
-        if int(list_bool[int(x)-1]) == 1:
-            y += 1
-    if y % 2 == 0:
-        return 1
-    else:
-        return 0 
-
-"""
+test()
